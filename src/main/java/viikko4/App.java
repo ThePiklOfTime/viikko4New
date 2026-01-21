033390447b05613efe4a626a5b51c1a7d8f5c65a
ThePiklOfTime <nicotolvanen@gmail.com>
2026-01-21 14:44:14 +0200

    Added menu

--- src/main/java/viikko4/App.java
+++ src/main/java/viikko4/App.java
@@ -1,5 +1,8 @@
 package viikko4;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,46 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Syötä pelaajan nimi: ");
+        String playerName = scanner.nextLine();
+        Player player = new Player(playerName);
+        Cave cave = new Cave(player);
+
+        while (!exit) {
+            System.out.println("1) Lisää luolaan hirviö \n2) Listaa hirviöt \n3) Hyökkää hirviöön \n4) Tallenna peli \n5) Lataa peli \n0) Lopeta ohjelma");
+            
+            if(scanner.hasNext()){
+                int i = Integer.parseInt(scanner.nextLine());
+            
+            switch (i) {
+                case 1:
+                    System.out.println("Syötä hirviön tyyppi: ");
+                    String type = scanner.nextLine();
+                    System.out.println("Syötä hirviön elämän määrä numerona: ");
+                    int health = Integer.parseInt(scanner.nextLine());
+                    Monster monster = new Monster(type, health);
+                    cave.addMonster(monster);
+                    break;
+                case 2:
+                    cave.listMonsters();
+                    break;
+                case 3:
+                    
+                    break;
+                case 0:
+                    
+                    break;
+                
+                default:
+                    System.out.println("Syöte oli väärä");
+                    break;
+            
+            
+                }
+        }
+    }
+    System.out.println("Kiitos ohjelman käytöstä.");
+    scanner.close();
     }
 }
